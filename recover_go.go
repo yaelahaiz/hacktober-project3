17ee2ca50d30b220effd3c9cf7a0eb23e2749a06
Mohammad Nowaf <muhammadnowaf2608@gmail.com>
2022-10-17 13:27:02 +0700

    Create recover_go.go
    recover in golang

---
+++ recover_go.go
@@ -0,0 +1,24 @@
+package main
+
+func catch() {
+    if r := recover(); r != nil {
+        fmt.Println("Error occured", r)
+    } else {
+        fmt.Println("Application running perfectly")
+    }
+}
+
+func main() {
+    defer catch()
+
+    var name string
+    fmt.Print("Type your name: ")
+    fmt.Scanln(&name)
+
+    if valid, err := validate(name); valid {
+        fmt.Println("halo", name)
+    } else {
+        panic(err.Error())
+        fmt.Println("end")
+    }
+}
